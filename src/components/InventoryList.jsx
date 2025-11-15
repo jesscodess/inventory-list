7f52e996bdd883b333fab8c4bdaeaa41b7b43a86
Jéssica Martins <jesscodes@proton.me>
2025-11-14 21:49:54 -0300

    Worked on the useReducer hook and general program logic.
    Trabalhei no hook useReducer e na lógica em geral do programa.

--- src/components/InventoryList.jsx
+++ src/components/InventoryList.jsx
@@ -1,6 +1,30 @@
 import React from 'react'
+import {useReducer, useEffect} from 'react'
+import { inventoryReducer, initialState } from '../reducers/InventoryReducer'
+import { FETCH_ACTIONS } from '../actions'
+import axios from 'axios'
 
 const InventoryList = () => {
+    const [state, dispatch] = useReducer(inventoryReducer, initialState);
+    const { items, loading, error } = state;
+    console.log(items, loading, error);
+    useEffect(() => {
+        dispatch({type: FETCH_ACTIONS.PROGRESS});
+        const getItems = async () => {
+            try{
+                let response = await axios.get("http://localhost:3000/edibles");
+                if (response.status === 200) {
+                    dispatch({type: FETCH_ACTIONS.SUCCESS, data: response.data});
+                }
+            } catch{
+                console.error(err);
+                dispatch({type: FETCH_ACTIONS.ERROR, error: err.message})
+            }
+        }
+
+        getItems();
+
+    }, []);
     return (
         <div>InventoryList</div>
     )
